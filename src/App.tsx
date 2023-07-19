dc13e20e611a25ff618a17af7a3444dcf2c49eb8
mwmarszalek <mw.marszalek@outlook.com>
2023-07-19 19:24:12 +0100

    fix endless blink bug

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,11 @@ import Links from "./components/Links";
 
 function App() {
   return (
-    <div style={{margin: '0'}}>
-    <div className="main-container">
-      <Logo />
-      <Links />
-    </div>
+    <div style={{ margin: "0" }}>
+      <div className="main-container">
+        <Logo />
+        <Links />
+      </div>
     </div>
   );
 }
--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,4 +1,5 @@
-import './Logo.css'
+import React, { useEffect, useState } from 'react';
+import './Logo.css';
 import P from '../assets/P.png';
 import A from '../assets/A.png';
 import L from '../assets/L.png';
@@ -6,16 +7,26 @@ import O from '../assets/O.png';
 import M from '../assets/M.png';
 
 const Logo: React.FC = () => {
-  
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoaded(true);
+    }, 500); // Adjust the delay as needed
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
 
   return (
-    <div className='logo-container'>
-      <img src={P} alt="P" className='logo-letters'/>
-      <img src={A} alt="A" className='logo-letters'/>
-      <img src={L} alt="L" className='logo-letters'/>
-      <img src={O} alt="O" className='logo-letters'/>
-      <img src={M} alt="M" className='logo-letters'/>
-      <img src={A} alt="A" className='logo-letters'/>
+    <div className={`logo-container ${isLoaded ? 'loaded' : ''}`}>
+      <img src={P} alt="P" className="logo-letters" />
+      <img src={A} alt="A" className="logo-letters" />
+      <img src={L} alt="L" className="logo-letters" />
+      <img src={O} alt="O" className="logo-letters" />
+      <img src={M} alt="M" className="logo-letters" />
+      <img src={A} alt="A" className="logo-letters" />
     </div>
   );
 };
