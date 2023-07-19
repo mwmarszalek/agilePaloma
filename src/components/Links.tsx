1687c2802a9957ceebdf0ca658daec5d3563bb03
mwmarszalek <mw.marszalek@outlook.com>
2023-07-19 11:15:52 +0100

    add glow animation on hover desktop

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -1,18 +1,33 @@
-import './Links.css'
+import React, { useEffect, useState } from 'react';
+import './Links.css';
 
 const Links: React.FC = () => {
+  const [blinked, setBlinked] = useState(false);
 
+  useEffect(() => {
+    const blinkLinks = async () => {
+      setBlinked(true);
+      await sleep(2000); // Adjust the duration of the blink (in milliseconds)
+      setBlinked(false);
+    };
+
+    blinkLinks();
+  }, []);
+
+  const sleep = (ms: number) => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  };
 
   return (
-    <div className='links-container'>
-      <div className='links'>HEAR</div>
-      <div className='links'>LATEST</div>
-      <div className='links'>TOUR</div>
-      <div className='links'>SEE</div>
-      <div className='links'>SHOP</div>
-      <div className='links'>WATCH</div>
-      <div className='links'>PERSONAL</div>
-      <div className='links'>AID</div>
+    <div className={`links-container ${blinked ? 'blinked' : ''}`}>
+      <a href="#" className="links">HEAR</a>
+      <a href="#" className="links">LATEST</a>
+      <a href="#" className="links">TOUR</a>
+      <a href="#" className="links">SEE</a>
+      <a href="#" className="links">SHOP</a>
+      <a href="#" className="links">WATCH</a>
+      <a href="#" className="links">PERSONAL</a>
+      <a href="#" className="links">AID</a>
     </div>
   );
 };
